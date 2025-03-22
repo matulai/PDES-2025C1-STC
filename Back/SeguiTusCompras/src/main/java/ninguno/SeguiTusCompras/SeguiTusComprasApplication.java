da56057c69e8f6b30dcb9c8c8c7113852a19fac9
Cabezas-Juan-Manuel <juancabezas95.jc@gmail.com>
2025-03-21 22:41:27 -0300

    se sube back

---
+++ Back/SeguiTusCompras/src/main/java/ninguno/SeguiTusCompras/SeguiTusComprasApplication.java
@@ -0,0 +1,13 @@
+package ninguno.SeguiTusCompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguiTusComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguiTusComprasApplication.class, args);
+	}
+
+}
---
+++ Back/SeguiTusCompras/src/test/java/ninguno/SeguiTusCompras/SeguiTusComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package ninguno.SeguiTusCompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguiTusComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
