34e43574f6a7d498f522b1801f32da6391f5b2a8
Matias Laime <matulai1221@gmail.com>
2025-03-23 20:41:44 -0300

    Se crea clase producto

---
+++ Back/SeguiTusCompras/src/main/java/ninguno/SeguiTusCompras/usuario/Producto.java
@@ -0,0 +1,38 @@
+package ninguno.SeguiTusCompras.usuario;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "productos")
+@Data
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String MLA_id;
+
+    @Column(nullable = false)
+    private String categoria_id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(precision = 12, scale = 3)
+    private BigDecimal precio;
+
+    @Column(nullable = false)
+    private String imagen_url;
+
+    @Column
+    private int cantidad;
+
+    @Column
+    private String description;
+
+}
