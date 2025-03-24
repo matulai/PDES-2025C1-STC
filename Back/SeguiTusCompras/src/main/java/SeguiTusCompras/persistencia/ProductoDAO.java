b1365edab53c0b9057de7e8e4fc864e3a68a83c2
Matias Laime <matulai1221@gmail.com>
2025-03-24 15:18:52 -0300

    Add: Se agrega CRUD al DAO de productos

--- Back/SeguiTusCompras/src/main/java/SeguiTusCompras/persistencia/ProductoDAO.java
+++ Back/SeguiTusCompras/src/main/java/SeguiTusCompras/persistencia/ProductoDAO.java
@@ -1,2 +1,8 @@
-package SeguiTusCompras.persistencia;public class productoDAO {
+package SeguiTusCompras.persistencia;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import SeguiTusCompras.modelo.Producto;
+
+interface ProductoDAO extends JpaRepository<Producto, Long> {
+
 }
