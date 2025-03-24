29b1715c89fef150ad562730aff7d02f4132c731
Matias Laime <matulai1221@gmail.com>
2025-03-23 21:01:58 -0300

    Refactor: modifico nombres de paquetes y elimine unos inncesarios

--- Back/SeguiTusCompras/src/main/java/SeguiTusCompras/SeguiTusComprasApplication.java
+++ Back/SeguiTusCompras/src/main/java/SeguiTusCompras/SeguiTusComprasApplication.java
@@ -1,4 +1,4 @@
-package ninguno.SeguiTusCompras;
+package SeguiTusCompras;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
