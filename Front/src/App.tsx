62d223fe0c5497d26a3e5d6d8da26eb5346cf2ff
Matias Laime <matulai1221@gmail.com>
2025-04-29 19:43:31 -0300

    Add: base css config finished

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "@/views";
 import MainLayout from "@/layout/MainLayout";
-import "./App.css";
 
 function App() {
   return (
