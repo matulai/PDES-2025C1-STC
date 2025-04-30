79aa7e28f41508b1f02af8768621e56cbebb109e
Matias Laime <matulai1221@gmail.com>
2025-04-30 10:10:36 -0300

    Add: Link to home on header

--- Front/src/components/Header.tsx
+++ Front/src/components/Header.tsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 import "@/styles/Header.css";
 
 const Header = () => {
   return (
     <header className="header-container">
-      <h1 className="header-container-title">STC</h1>
+      <h1 className="header-container-title">
+        <Link to="/" className="header-container-title-link">
+          STC
+        </Link>
+      </h1>
       <Navbar />
     </header>
   );
