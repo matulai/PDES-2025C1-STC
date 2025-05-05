7dd2299dd9476031b1ea604f788b2458afe1218f
Matias Laime <matulai1221@gmail.com>
2025-05-05 16:53:46 -0300

    Add: carousel scrollable component finished

--- Front/src/components/ScrollableCarousel.tsx
+++ Front/src/components/ScrollableCarousel.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import ChevronLeftIcon from "./ChevronLeftIcon";
 import "@/styles/ScrollableCarousel.css";
 
@@ -6,13 +7,35 @@ interface ScrollableCarouselProps {
 }
 
 const ScrollableCarousel = ({ children }: ScrollableCarouselProps) => {
+  const carouselContainerRef = useRef<HTMLDivElement>(null);
+
+  const handleOnClickScroll = (direction: "left" | "right") => {
+    const current = carouselContainerRef.current;
+    if (!current) return;
+
+    const scrollAmount = current.clientWidth + 24;
+
+    current.scrollBy({
+      left: direction === "left" ? -scrollAmount : scrollAmount,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <>
-      <button className="scrollable-carousel-button scrollable-carousel-button-prev">
+      <button
+        className="scrollable-carousel-button scrollable-carousel-button-prev"
+        onClick={() => handleOnClickScroll("left")}
+      >
         <ChevronLeftIcon />
       </button>
-      <div className="scrollable-carousel-container">{children}</div>
-      <button className="scrollable-carousel-button scrollable-carousel-button-next">
+      <div className="scrollable-carousel-container" ref={carouselContainerRef}>
+        <div className="scrollable-carousel-container-content">{children}</div>
+      </div>
+      <button
+        className="scrollable-carousel-button scrollable-carousel-button-next"
+        onClick={() => handleOnClickScroll("right")}
+      >
         <ChevronLeftIcon style={{ transform: "rotate(180deg)" }} />
       </button>
     </>
