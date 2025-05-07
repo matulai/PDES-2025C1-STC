2d2161a7d7fb956c5307f9f720b435d9632464cf
Matias Laime <matulai1221@gmail.com>
2025-05-06 23:28:24 -0300

    Add: auth service

---
+++ Front/src/service/authService.tsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+function login(username: string, password: string) {
+  return axios.post(`${API_URL}/auth/login`, { username, password });
+}
+
+function register(username: string, password: string) {
+  return axios.post(`${API_URL}/auth/register`, { username, password });
+}
+
+export { login, register };
