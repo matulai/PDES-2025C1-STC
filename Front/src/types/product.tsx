12ac76b8d5e9f08d963d13ba83fcdc1d2bd365fa
Matias Laime <matulai1221@gmail.com>
2025-05-06 23:29:38 -0300

    Add: product type

---
+++ Front/src/types/product.tsx
@@ -0,0 +1,11 @@
+interface Product {
+  id?: string;
+  MLId: string;
+  name: string;
+  description?: string;
+  imageUrl: string;
+  qualify?: number;
+  domain_id?: string;
+}
+
+export default Product;
