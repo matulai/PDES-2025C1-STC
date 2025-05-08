ce9a65569060f076e2965da3c222c14ead882de6
Matias Laime <matulai1221@gmail.com>
2025-05-07 23:35:59 -0300

    Refactor: add columns restrictions and add a property

--- Back/SeguiTusCompras/src/main/java/SeguiTusCompras/model/Qualification.java
+++ Back/SeguiTusCompras/src/main/java/SeguiTusCompras/model/Qualification.java
@@ -2,25 +2,37 @@ package SeguiTusCompras.model;
 
 import SeguiTusCompras.model.user.Client;
 import jakarta.persistence.Entity;
+import lombok.NoArgsConstructor;
 import jakarta.persistence.Id;
 import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class Qualification {
+
+    public Qualification(Client client, Product product, Integer score) {
+        this.client = client;
+        this.product = product;
+        this.score = score;
+    }
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "qualification_id")
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name= "client_id")
+    @JoinColumn(name= "client_id", nullable = false)
     private Client client;
 
     @ManyToOne
-    @JoinColumn(name = "product_id")
+    @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
-    private Integer score;
+    private String comment;
 
-    public Qualification(Client client, Product product, Integer score) {}
+    @Column(nullable = false)
+    private Integer score;
 }
