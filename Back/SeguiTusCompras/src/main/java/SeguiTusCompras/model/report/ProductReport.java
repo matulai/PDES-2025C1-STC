ca0bc1465745c1a72c193b02f6b8f8cd352ffee8
Cabezas-Juan-Manuel <juancabezas95.jc@gmail.com>
2025-05-13 10:29:23 -0300

    funcionan los endpoints

--- Back/SeguiTusCompras/src/main/java/SeguiTusCompras/model/report/ProductReport.java
+++ Back/SeguiTusCompras/src/main/java/SeguiTusCompras/model/report/ProductReport.java
@@ -7,9 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
