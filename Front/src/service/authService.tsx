b0c885f23dd2b149c1376c70bceeaa0eba4ddab6
Matias Laime <matulai1221@gmail.com>
2025-05-14 00:02:57 -0300

    Add: auth logic for register and login

--- Front/src/service/authService.tsx
+++ Front/src/service/authService.tsx
@@ -2,12 +2,19 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-function login(username: string, password: string) {
-  return axios.post(`${API_URL}/auth/login`, { username, password });
+function login(name: string, password: string) {
+  return axios.post(`${API_URL}/auth/login`, {
+    name: name,
+    password: password,
+  });
 }
 
-function register(username: string, password: string, role: string) {
-  return axios.post(`${API_URL}/auth/register`, { username, password, role });
+function register(name: string, password: string, role: string) {
+  return axios.post(`${API_URL}/auth/register`, {
+    name: name,
+    password: password,
+    role: role,
+  });
 }
 
 export { login, register };
--- Front/src/views/Login.tsx
+++ Front/src/views/Login.tsx
@@ -1,9 +1,26 @@
 import { LoginCard } from "@/components";
+import { login } from "@/service/authService";
 
 const Login = () => {
-  const handleLogin = () => {
-    console.log("Login");
-    // hacer login...
+  const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const username = (form.elements.namedItem("username") as HTMLInputElement)
+      ?.value;
+    const password = (form.elements.namedItem("password") as HTMLInputElement)
+      ?.value;
+
+    if (!username || !password) {
+      alert("Por favor completá todos los campos.");
+      return;
+    }
+    login(username, password)
+      .then(response => {
+        console.log("Login successful", response.data);
+      })
+      .catch(error => {
+        console.error("Login failed", error);
+      });
   };
 
   return (
