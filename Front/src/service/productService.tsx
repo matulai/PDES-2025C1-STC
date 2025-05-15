aad188331f17f28564b0b4c69d0f0e9d2bde7c4b
Matias Laime <matulai1221@gmail.com>
2025-05-14 23:55:40 -0300

    Refactor: change on endpoint from backend so that

--- Front/src/service/productService.tsx
+++ Front/src/service/productService.tsx
@@ -5,21 +5,34 @@ const PUBLIC_API_URL = import.meta.env.VITE_PUBLIC_API_URL;
 
 /********************* Private requests *********************/
 
-function getProductsByKeyword(query: string, domain_id?: string) {
-  const params: Record<string, string> = { q: query };
-  if (domain_id) params.domain_id = domain_id;
+/********************* Public requests *********************/
 
-  return axios.get(`${API_URL}/products/search`, { params });
+function getProductsByKeyword(query: string) {
+  return axios.get(`${API_URL}/products/search`, {
+    params: { text: query },
+  });
+}
+
+function getProductsByKeywordAndCategory(query: string, categoryId: string) {
+  return axios.get(`${API_URL}/products/search`, {
+    params: {
+      text: query,
+      category: categoryId,
+    },
+  });
 }
 
 function getProductById(id: string) {
   return axios.get(`${API_URL}/products/${id}`);
 }
 
-/********************* Public requests *********************/
-
 function getCategoryById(id: string) {
   return axios.get(`${PUBLIC_API_URL}/categories/${id}`);
 }
 
-export { getProductsByKeyword, getProductById, getCategoryById };
+export {
+  getProductById,
+  getCategoryById,
+  getProductsByKeyword,
+  getProductsByKeywordAndCategory,
+};
