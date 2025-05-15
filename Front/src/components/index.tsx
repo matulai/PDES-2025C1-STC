c78801c1b0b2f3dd6f79b7cdab081a306444517e
Matias Laime <matulai1221@gmail.com>
2025-05-14 23:53:17 -0300

    Add: logic for connect to API on carousel

--- Front/src/components/index.tsx
+++ Front/src/components/index.tsx
@@ -1,5 +1,5 @@
 export { default as Footer } from "./Footer";
 export { default as Header } from "./Header";
-export { default as Carousel } from "./Carousel";
-export { default as RegisterCard } from "./RegisterCard";
 export { default as LoginCard } from "./LoginCard";
+export { default as RegisterCard } from "./RegisterCard";
+export { default as CarouselContainer } from "./CarouselContainer";
