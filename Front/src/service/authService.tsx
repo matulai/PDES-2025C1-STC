823ba470ffdac899cbf813e2e71bb7a4e236faaa
Matias Laime <matulai1221@gmail.com>
2025-05-15 23:04:12 -0300

    Fix: types on authService

--- Front/src/service/authService.tsx
+++ Front/src/service/authService.tsx
@@ -1,16 +1,17 @@
 import axios from "axios";
+import { User } from "@/types";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 function login(name: string, password: string) {
-  return axios.post(`${API_URL}/auth/login`, {
+  return axios.post<User>(`${API_URL}/auth/login`, {
     name: name,
     password: password,
   });
 }
 
 function register(name: string, password: string, role: string) {
-  return axios.post(`${API_URL}/auth/register`, {
+  return axios.post<User>(`${API_URL}/auth/register`, {
     name: name,
     password: password,
     role: role,
