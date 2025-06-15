29ae6fe37d259f74ec00998d7ef28cda6f5c6974
Matias Laime <matulai1221@gmail.com>
2025-06-14 22:55:35 -0300

    Delete: product report

--- Back/SeguiTusCompras/src/main/java/SeguiTusCompras/persistence/report/IReportDao.java
+++
@@ -1,9 +0,0 @@
-package SeguiTusCompras.persistence.report;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import SeguiTusCompras.model.report.Report;
-
-public interface IReportDao extends JpaRepository<Report, Long>{
-    
-}
