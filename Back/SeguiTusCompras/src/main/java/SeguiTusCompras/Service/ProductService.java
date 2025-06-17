e62407da090fb84efe94e3ceadba337a4d2b224d
Matias Laime <matulai1221@gmail.com>
2025-06-16 23:31:54 -0300

    Refactor: new constructor and delete unnused report

--- Back/SeguiTusCompras/src/main/java/SeguiTusCompras/Service/ProductService.java
+++ Back/SeguiTusCompras/src/main/java/SeguiTusCompras/Service/ProductService.java
@@ -2,26 +2,24 @@ package SeguiTusCompras.Service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import SeguiTusCompras.model.Product;
-import SeguiTusCompras.model.report.ProductReport;
 import SeguiTusCompras.persistence.IProductDao;
 import org.springframework.data.domain.Pageable;
 
 @Service
 public class ProductService {
-    @Autowired
-    IProductDao productDao;
+    private final IProductDao productDao;
+
+    public ProductService(IProductDao productDao) {
+        this.productDao = productDao;
+    }
 
     public Product getProduct(Product product) {
         Product persistedProduct = productDao.getByName(product.getName());
         if (persistedProduct == null){
-            Product newProduct = productDao.save(product);
-            ProductReport productReport = new ProductReport();
-            productReport.setProduct(newProduct);
-            return productDao.save(newProduct);
+            return productDao.save(product);
         }
         return persistedProduct;    
     }
@@ -30,7 +28,7 @@ public class ProductService {
         return productDao.getByName(productName);
     }
 
-    public Product getProductByMlaId(String id) { return productDao.getByMlaId(id);}
+    public Product getProductByMlaId(String id) { return productDao.findByMlaId(id).orElse(null);}
 
     public List<Product> getAllFavoritesProducts() {
         return productDao.findAllFavoriteProductsOfAllUsers();
