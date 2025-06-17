9757338e26ed77bccb84d04b2ece77f5fc286b84
Matias Laime <matulai1221@gmail.com>
2025-06-16 23:29:02 -0300

    Refactor: name change because JPA dont give the method what i want

--- Back/SeguiTusCompras/src/main/java/SeguiTusCompras/persistence/IProductDao.java
+++ Back/SeguiTusCompras/src/main/java/SeguiTusCompras/persistence/IProductDao.java
@@ -6,13 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IProductDao extends JpaRepository<Product, Long> {
     @Query("SELECT p FROM Product p WHERE p.name = ?1")
     Product getByName(String productName);
 
     @Query("SELECT p FROM Product p WHERE p.mlaId = ?1")
-    Product getByMlaId(String id);
+    Optional<Product> findByMlaId(String id);
 
     @Query("SELECT DISTINCT p FROM User u JOIN u.favorites p")
     List<Product> findAllFavoriteProductsOfAllUsers();
