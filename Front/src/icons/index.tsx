d509950dce1b1e06febd9258121b08722939579c
Matias Laime <matulai1221@gmail.com>
2025-06-16 23:25:24 -0300

    Add: trash icon

--- Front/src/icons/index.tsx
+++ Front/src/icons/index.tsx
@@ -2,6 +2,7 @@ export { default as EyeIcon } from "./EyeIcon";
 export { default as SendIcon } from "./SendIcon";
 export { default as StarIcon } from "./StarIcon";
 export { default as CartIcon } from "./CartIcon";
+export { default as TrashIcon } from "./TrashIcon";
 export { default as EyeOffIcon } from "./EyeOffIcon";
 export { default as ChevronDownIcon } from "./ChevronDownIcon";
 export { default as ChevronLeftIcon } from "./ChevronLeftIcon";
