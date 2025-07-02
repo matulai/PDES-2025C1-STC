d3c0dfa4086be0aee61c2c52b5ef3f98a736a150
Matias Laime <matulai1221@gmail.com>
2025-07-01 22:13:26 -0300

    Add: pagination on Qualification view

--- Front/src/views/Qualifications.tsx
+++ Front/src/views/Qualifications.tsx
@@ -1,19 +1,22 @@
+import type { Qualification, PaginationElementDto } from "@/types";
 import { allUsersQualifications } from "@/service/adminService";
+import { Spinner, PaginationNav } from "@/components";
 import { useState, useEffect } from "react";
-import type { Qualification } from "@/types";
-import { Spinner } from "@/components";
+import { useSearchParams } from "react-router-dom";
 import "@/styles/Items.css";
 
 const Qualifications = () => {
-  const [usersQualifications, setUsersQualifications] = useState<
-    Qualification[]
-  >([]);
+  const [searchParams] = useSearchParams();
+  const [paginationUsersQualifications, setPaginationUsersQualifications] =
+    useState<PaginationElementDto<Qualification>>();
   const [isLoading, setIsLoading] = useState(true);
 
+  const page = Number(searchParams.get("page"));
+
   useEffect(() => {
-    allUsersQualifications()
+    allUsersQualifications(page)
       .then(res => {
-        setUsersQualifications(res.data.data);
+        setPaginationUsersQualifications(res);
       })
       .catch(error => {
         console.log(error);
@@ -30,26 +33,28 @@ const Qualifications = () => {
   return (
     <>
       <h1 className="items-title">
-        <strong style={{ fontWeight: "600" }}>All users qualifications</strong>
+        <strong style={{ fontWeight: "600" }}>Todas las calificaciones</strong>
       </h1>
       <div className="items">
         <div className="items-content-column">
-          {usersQualifications.map((userQualifications, index) => (
-            <div key={index} className="items-content-item">
-              <p>
-                <b style={{ fontWeight: "600" }}>
-                  {userQualifications.userName}
-                </b>{" "}
-                hizo un comentario en el producto:{" "}
-                {userQualifications.productName}
-              </p>
-              <p>comentario: "{userQualifications.comment}"</p>
-              <p>score: {userQualifications.score}</p>
-            </div>
-          ))}
+          {paginationUsersQualifications?.data.map(
+            (userQualifications, index) => (
+              <div key={index} className="items-content-item">
+                <p>
+                  <b style={{ fontWeight: "600" }}>
+                    {userQualifications.userName}
+                  </b>{" "}
+                  hizo un comentario en el producto:{" "}
+                  {userQualifications.productName}
+                </p>
+                <p>comentario: "{userQualifications.comment}"</p>
+                <p>puntuación: {userQualifications.score}</p>
+              </div>
+            )
+          )}
         </div>
-        {/* <Pagination products={products.pagination}/> */}
       </div>
+      <PaginationNav pagination={paginationUsersQualifications!.pagination} />
     </>
   );
 };
