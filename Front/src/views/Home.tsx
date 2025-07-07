7133c47046db84d7db26ef39ef8e600a72757301
Matias Laime <matulai1221@gmail.com>
2025-07-07 13:34:01 -0300

    Add: more sections of products on home page

--- Front/src/views/Home.tsx
+++ Front/src/views/Home.tsx
@@ -3,9 +3,19 @@ const Home = () => {
   return (
     <>
       <ProductsCategoryContainer
-        link="/"
+        link="/search/Samsung"
         title="Samsung"
         category="MLA-CELLPHONES"
+      />
+      <ProductsCategoryContainer
+        link="search/Libros"
+        title="Libros"
+        category="MLA-BOOKS"
+      />
+      <ProductsCategoryContainer
+        link="search/Computadoras"
+        title="Computadoras"
+        category="MLA-DESKTOP_COMPUTER_KITS"
       />
     </>
   );
