617f7606c73d64faaa8c9b4a0ab5f4171f42d667
Cabezas-Juan-Manuel <juancabezas95.jc@gmail.com>
2025-07-10 01:15:32 -0300

    data seeder para stg

--- Back/SeguiTusCompras/src/main/java/SeguiTusCompras/model/Product.java
+++ Back/SeguiTusCompras/src/main/java/SeguiTusCompras/model/Product.java
@@ -2,8 +2,10 @@ package SeguiTusCompras.model;
 
 import jakarta.persistence.*;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
